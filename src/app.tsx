47d9615ac5d1e3071b2896e79ab5865b461e72b1
Filipe Bezerra <felipe.vitorio.bz@gmail.com>
2024-02-07 19:18:24 -0300

    Implementa primeiro componente

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,13 @@
+import logo from './assets/logo-nlw-white.svg'
+
 export function App() {
-  return <h1>Olá</h1>
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-emerald-400 underline">
+        Hello world!
+      </h1>
+
+      <img src={logo} className="w-52 h-w-52 mx-auto" />
+    </div>
+  )
 }
